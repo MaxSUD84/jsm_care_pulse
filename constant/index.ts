cee0ec3529cb929c71cf39739044600ff503789d
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2024-07-24 15:38:17 +0300

    UPD: some utils & validator & types

--- constant/index.ts
+++ constant/index.ts
@@ -1,86 +1,84 @@
-export const GenderOptions = ["Male", "Female", "Other"];
+export const GenderOptions = ['Муж', 'Жен'];
 
 export const PatientFormDefaultValues = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    birthDate: new Date(Date.now()),
-    gender: "Male" as Gender,
-    address: "",
-    occupation: "",
-    emergencyContactName: "",
-    emergencyContactNumber: "",
-    primaryPhysician: "",
-    insuranceProvider: "",
-    insurancePolicyNumber: "",
-    allergies: "",
-    currentMedication: "",
-    familyMedicalHistory: "",
-    pastMedicalHistory: "",
-    identificationType: "Birth Certificate",
-    identificationNumber: "",
-    identificationDocument: [],
-    treatmentConsent: false,
-    disclosureConsent: false,
-    privacyConsent: false,
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  birthDate: new Date(Date.now()),
+  gender: 'Муж' as Gender,
+  address: '',
+  occupation: '',
+  emergencyContactName: '',
+  emergencyContactNumber: '',
+  primaryPhysician: '',
+  insuranceProvider: '',
+  insurancePolicyNumber: '',
+  allergies: '',
+  currentMedication: '',
+  familyMedicalHistory: '',
+  pastMedicalHistory: '',
+  identificationType: 'Свидетельство о рождении',
+  identificationNumber: '',
+  identificationDocument: [],
+  treatmentConsent: false,
+  disclosureConsent: false,
+  privacyConsent: false,
 };
 
 export const IdentificationTypes = [
-    "Birth Certificate",
-    "Driver's License",
-    "Medical Insurance Card/Policy",
-    "Military ID Card",
-    "National Identity Card",
-    "Passport",
-    "Resident Alien Card (Green Card)",
-    "Social Security Card",
-    "State ID Card",
-    "Student ID Card",
-    "Voter ID Card",
+  'Свидетельство о рождении',
+  'Водительские права',
+  'Медицинский полис',
+  'Военный билет',
+  'Загран паспорт',
+  'Паспорт',
+  'Разрешение на работу (виза)',
+  'СНИЛС',
+  'Студенческий билет',
 ];
 
 export const Doctors = [
-    {
-        image: "/assets/images/dr-green.png",
-        name: "John Green",
-    },
-    {
-        image: "/assets/images/dr-cameron.png",
-        name: "Leila Cameron",
-    },
-    {
-        image: "/assets/images/dr-livingston.png",
-        name: "David Livingston",
-    },
-    {
-        image: "/assets/images/dr-peter.png",
-        name: "Evan Peter",
-    },
-    {
-        image: "/assets/images/dr-powell.png",
-        name: "Jane Powell",
-    },
-    {
-        image: "/assets/images/dr-remirez.png",
-        name: "Alex Ramirez",
-    },
-    {
-        image: "/assets/images/dr-lee.png",
-        name: "Jasmine Lee",
-    },
-    {
-        image: "/assets/images/dr-cruz.png",
-        name: "Alyana Cruz",
-    },
-    {
-        image: "/assets/images/dr-sharma.png",
-        name: "Hardik Sharma",
-    },
+  {
+    image: '/assets/images/dr-green.png',
+    name: 'Евгений Зеленый',
+  },
+  {
+    image: '/assets/images/dr-cameron.png',
+    name: 'Елена Камеронова',
+  },
+  {
+    image: '/assets/images/dr-livingston.png',
+    name: 'Давид Каменщиков',
+  },
+  {
+    image: '/assets/images/dr-peter.png',
+    name: 'Иван Петров',
+  },
+  {
+    image: '/assets/images/dr-powell.png',
+    name: 'Жанна Повелевская',
+  },
+  {
+    image: '/assets/images/dr-remirez.png',
+    name: 'Алексей Ремирезов',
+  },
+  {
+    image: '/assets/images/dr-lee.png',
+    name: 'Жасмина Ли',
+  },
+  {
+    image: '/assets/images/dr-cruz.png',
+    name: 'Алена Крузина',
+  },
+  {
+    image: '/assets/images/dr-sharma.png',
+    name: 'Харитон Шарманов',
+  },
 ];
 
 export const StatusIcon = {
-    scheduled: "/assets/icons/check.svg",
-    pending: "/assets/icons/pending.svg",
-    cancelled: "/assets/icons/cancelled.svg",
+  scheduled: '/assets/icons/check.svg',
+  pending: '/assets/icons/pending.svg',
+  cancelled: '/assets/icons/cancelled.svg',
 };
